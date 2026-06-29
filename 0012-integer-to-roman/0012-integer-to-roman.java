4e868812f30478b152b4d481de6984d80ca684d4
deepikabalhara <deepikabalhara00@gmail.com>
2026-06-29 20:16:22 +0530

    Time: 2 ms (100%), Space: 46.6 MB (26.23%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,9 +1,45 @@
 class Solution {
     public static String intToRoman(int num) {
-    String M[] = {"", "M", "MM", "MMM"};
-    String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-    String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-    String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-    return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+        char[] buf = new char[15];
+        int pos = 0;
+        int thousands = num / 1000;
+        while (thousands-- > 0) {
+            buf[pos++] = 'M';
+        }
+        int hundreds = (num % 1000) / 100;
+        if (hundreds == 9) {
+            buf[pos++] = 'C'; buf[pos++] = 'M';
+        } else if (hundreds >= 5) {
+            buf[pos++] = 'D';
+            while (hundreds-- > 5) buf[pos++] = 'C';
+        } else if (hundreds == 4) {
+            buf[pos++] = 'C'; buf[pos++] = 'D';
+        } else {
+            while (hundreds-- > 0) buf[pos++] = 'C';
+        }
+        int tens = (num % 100) / 10;
+        if (tens == 9) {
+            buf[pos++] = 'X'; buf[pos++] = 'C';
+        } else if (tens >= 5) {
+            buf[pos++] = 'L';
+            while (tens-- > 5) buf[pos++] = 'X';
+        } else if (tens == 4) {
+            buf[pos++] = 'X'; buf[pos++] = 'L';
+        } else {
+            while (tens-- > 0) buf[pos++] = 'X';
+        }
+        int ones = num % 10;
+        if (ones == 9) {
+            buf[pos++] = 'I'; buf[pos++] = 'X';
+        } else if (ones >= 5) {
+            buf[pos++] = 'V';
+            while (ones-- > 5) buf[pos++] = 'I';
+        } else if (ones == 4) {
+            buf[pos++] = 'I'; buf[pos++] = 'V';
+        } else {
+            while (ones-- > 0) buf[pos++] = 'I';
+        }
+        return new String(buf, 0, pos);
     }
+
 }
